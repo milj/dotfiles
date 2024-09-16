aa6e8c0be47e187de687d2c4e87cbaa98ae5af54
Miłosz Jerkiewicz <milosz@xcnt.io>
2024-09-16 13:00:44 +0200

    Add screenlayout files

---
+++ screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 3840x1538 --rotate normal --output HDMI-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-3 --off --output DP-4 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
